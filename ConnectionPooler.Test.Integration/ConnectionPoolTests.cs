0ef24ea9fdc3c7cae05019c82de1f820760dc18f
William Davis <william.j.davis5@gmail.com>
2019-12-28 11:01:22 -0500

    Added integration tests

---
+++ ConnectionPooler.Test.Integration/ConnectionPoolTests.cs
@@ -0,0 +1,42 @@
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RedisPooler;
+using StackExchange.Redis;
+
+namespace ConnectionPooler.Test.Integration
+{
+    [TestClass]
+    public class ConnectionPoolTests
+    {
+        [TestMethod]
+        public void Returns_Successful_Connection_To_Localhost_Redis_6379()
+        {
+            var config = new ConfigurationOptions();
+            config.EndPoints.Add("127.0.0.1",6379);
+            var arbitraryPoolSize = 2;
+            var pooler = new ConnectionPool(arbitraryPoolSize, config);
+            var connection = pooler.GetConnection();
+            connection.Should().BeOfType<ConnectionMultiplexer>();
+        }
+
+        [TestMethod]
+        public void Returns_Successful_Connection_To_Localhost_Redis_6379_And_Can_Run_Commands()
+        {
+            var config = new ConfigurationOptions();
+            config.EndPoints.Add("127.0.0.1", 6379);
+            var arbitraryPoolSize = 2;
+            var pooler = new ConnectionPool(arbitraryPoolSize, config);
+            var connection = pooler.GetConnection();
+            connection.Should().BeOfType<ConnectionMultiplexer>();
+            var db = connection.GetDatabase();
+
+            var arbitraryvalue = "ArbitraryValue";
+            var arbitrarykeyname = "ArbitraryKeyName";
+            var setResult = db.StringSet(arbitrarykeyname, arbitraryvalue);
+            setResult.Should().BeTrue();
+            var getResult = db.StringGet(arbitrarykeyname).ToString();
+            getResult.Should().Match(arbitraryvalue);
+
+        }
+    }
+}
