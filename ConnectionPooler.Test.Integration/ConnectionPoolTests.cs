0379ef670c50ce43f53abfd1fecdd10cc6fd52e0
wjdavis5 <william.j.davis5@gmail.com>
2020-09-30 12:54:24 -0400

    bump versions

--- ConnectionPooler.Test.Integration/ConnectionPoolTests.cs
+++ ConnectionPooler.Test.Integration/ConnectionPoolTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RedisPooler;
@@ -12,7 +13,7 @@ namespace ConnectionPooler.Test.Integration
         public void Returns_Successful_Connection_To_Localhost_Redis_6379()
         {
             var config = new ConfigurationOptions();
-            config.EndPoints.Add("127.0.0.1",6379);
+            config.EndPoints.Add("127.0.0.1", 6379);
             var arbitraryPoolSize = 2;
             var pooler = new ConnectionPool(arbitraryPoolSize, config);
             var connection = pooler.GetConnection();
@@ -24,19 +25,21 @@ namespace ConnectionPooler.Test.Integration
         {
             var config = new ConfigurationOptions();
             config.EndPoints.Add("127.0.0.1", 6379);
-            var arbitraryPoolSize = 2;
+            var arbitraryPoolSize = 20;
             var pooler = new ConnectionPool(arbitraryPoolSize, config);
             var connection = pooler.GetConnection();
             connection.Should().BeOfType<ConnectionMultiplexer>();
             var db = connection.GetDatabase();
 
-            var arbitraryvalue = "ArbitraryValue";
-            var arbitrarykeyname = "ArbitraryKeyName";
-            var setResult = db.StringSet(arbitrarykeyname, arbitraryvalue);
-            setResult.Should().BeTrue();
-            var getResult = db.StringGet(arbitrarykeyname).ToString();
-            getResult.Should().Match(arbitraryvalue);
-
+            Enumerable.Range(1, 100000).ToList().AsParallel().WithDegreeOfParallelism(10).ForAll(i =>
+            {
+                var arbitraryvalue = "ArbitraryValue";
+                var arbitrarykeyname = $"ArbitraryKeyName:{i}";
+                var setResult = db.StringSet(arbitrarykeyname, arbitraryvalue);
+                setResult.Should().BeTrue();
+                var getResult = db.StringGet(arbitrarykeyname).ToString();
+                getResult.Should().Match(arbitraryvalue);
+            });
         }
     }
 }
